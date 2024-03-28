4f69c2ff34b577925871c948dd0e744cc42fe12f
callmeeve <wididiky38@gmail.com>
2024-03-28 13:58:22 +0700

    attendance

---
+++ pages/api/employee/attendance/all.js
@@ -0,0 +1,54 @@
+import { prisma } from "@/config/db";
+import jwt from "jsonwebtoken";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+
+  if (decoded.role !== "EMPLOYEE") {
+    return res.status(403).json({ message: "You are not authorized" });
+  }
+
+  let employee;
+  try {
+    employee = await prisma.employee.findUnique({
+      where: {
+        userId: decoded.id,
+      },
+      include: {
+        Manager: true,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error fetching employee" });
+  }
+
+  if (!employee) {
+    return res.status(404).json({ message: "Employee not found" });
+  }
+
+  const attendances = await prisma.attendance.findMany({
+    where: {
+      employeeId: employee.id,
+    },
+    orderBy: {
+      date: "desc",
+    },
+  });
+
+  return res.status(200).json(attendances);
+}
