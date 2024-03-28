5c14ed9e713e2b4de35f6a420531c2dc8e715019
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-28 15:41:54 +0700

    Update ManagerLayout.js

--- components/layouts/ManagerLayout.js
+++ components/layouts/ManagerLayout.js
@@ -3,7 +3,12 @@ import { useRouter } from "next/router";
 import Sidebar from "../partials/Sidebar";
 import { Popover } from "@headlessui/react";
 import Image from "next/image";
-import { IoChevronDown, IoLogOutOutline, IoPersonCircleOutline } from "react-icons/io5";
+import {
+  IoChevronDown,
+  IoLogOutOutline,
+  IoPersonCircleOutline,
+  IoNotifications,
+} from "react-icons/io5";
 import Link from "next/link";
 
 function ManagerLayout({ children }) {
@@ -42,7 +47,44 @@ function ManagerLayout({ children }) {
           <div className="flex items-center justify-between">
             <div className="flex flex-col items-start">
               <h1 className="font-medium">Welcome, {users.username}</h1>
-              <span className="text-gray-500 text-sm lowercase">{users.role}</span>
+              <span className="text-gray-500 text-sm lowercase">
+                {users.role}
+              </span>
+            </div>
+            <div className="relative ml-auto mr-4">
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button className="flex items-center justify-center focus:outline-none">
+                      <IoNotifications className="text-gray-800 w-6 h-6" />
+                    </Popover.Button>
+                    <Popover.Panel
+                      className={`${
+                        open ? "block" : "hidden"
+                      } absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg`}
+                    >
+                      <ul className="flex flex-col gap-y-2 p-2">
+                        <li>
+                          <a
+                            href="#"
+                            className="w-full flex items-center text-gray-800 hover:bg-blue-500 hover:text-white px-2 py-1 rounded-md"
+                          >
+                            <span className="text-sm">Notification 1</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="w-full flex items-center text-gray-800 hover:bg-blue-500 hover:text-white px-2 py-1 rounded-md"
+                          >
+                            <span className="text-sm">Notification 2</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </Popover.Panel>
+                  </>
+                )}
+              </Popover>
             </div>
             <Popover className="relative">
               {({ open }) => (
@@ -64,7 +106,10 @@ function ManagerLayout({ children }) {
                   >
                     <ul className="flex flex-col gap-y-2 p-2">
                       <li>
-                        <Link href="/manager/profile" className="w-full flex items-center text-gray-800 hover:bg-blue-500 hover:text-white px-2 py-1 rounded-md">
+                        <Link
+                          href="/manager/profile"
+                          className="w-full flex items-center text-gray-800 hover:bg-blue-500 hover:text-white px-2 py-1 rounded-md"
+                        >
                           <IoPersonCircleOutline className="w-6 h-6 mr-2" />
                           <span className="text-sm">Profile</span>
                         </Link>
